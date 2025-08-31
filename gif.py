b9e6382f0cd5e2c7a4e4323f1267fd8e68ca7426
Geovana Franca <43387470+Geovanaafs@users.noreply.github.com>
2025-08-31 12:53:56 -0400

    Update gif.py

--- gif.py
+++ gif.py
@@ -1,6 +1,5 @@
 import imageio.v3 as iio
 
-
 filenames = ['cat1.png','cat2.png','cat3.png', 'cat4.png', 'cat5.png', 'cat6.png', 'cat7.png','cat6.png','cat5.png', 'cat4.png','cat3.png','cat2.png']
 images = [ ]
 
@@ -8,4 +7,3 @@ for filename in filenames:
   images.append(iio.imread(filename))
 
 iio.imwrite('cat.gif', images, duration = 500, loop = 0)
-# iio.imwrite('team.gif', images, duration = 500, loop = 0)
